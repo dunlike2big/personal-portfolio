df21b276d406db487b15a5623d3ff544e6924ee7
ZJ <dunlike2big@hotmail.com>
2021-08-03 22:19:12 -0400

    Minor changes for smaller screen size

--- public/javascript/3-line-menu.js
+++ public/javascript/3-line-menu.js
@@ -4,17 +4,17 @@ function goToFirstPage() {
    // time out to fix issue where it would not scroll to specific part of page while using offcanvas from bootstrap
    setTimeout(function () {
       document.getElementById("firstPage").scrollIntoView();
-   }, 500);
+   }, 400);
 }
 
 function goToSecondPage() {
    setTimeout(function () {
       document.getElementById("secondPage").scrollIntoView();
-   }, 500);
+   }, 400);
 }
 
 function goToThirdPage() {
    setTimeout(function () {
       document.getElementById("lastPage").scrollIntoView();
-   }, 500);
+   }, 400);
 }
